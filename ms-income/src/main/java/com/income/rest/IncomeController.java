860d762d7f7fdef2ce14dd90f047189ba4d08ac2
maiconmoretto <maiconmorettos@gmail.com>
2022-03-21 11:17:57 -0300

    Added swagger to income.

--- ms-income/src/main/java/com/income/rest/IncomeController.java
+++ ms-income/src/main/java/com/income/rest/IncomeController.java
@@ -2,6 +2,8 @@ package com.income.rest;
 
 import com.income.entity.Income;
 import com.income.service.IncomeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,16 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Api(value = "income")
 public class IncomeController {
 
     @Autowired
     IncomeService IncomeService;
 
+
+    @ApiOperation(value = "List all incomes")
     @GetMapping("/income")
     public List<Income> findAll() {
         return IncomeService.findAll();
     }
 
+    @ApiOperation(value = "Save a income")
     @PostMapping("/income")
     public Income save(@RequestBody Income Income) throws Exception {
         return IncomeService.save(Income);
