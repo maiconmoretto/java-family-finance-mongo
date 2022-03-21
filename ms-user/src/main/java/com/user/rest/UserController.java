1389004f1efa52a9b51bcbef5881d8b4c57ecb8b
maiconmoretto <maiconmorettos@gmail.com>
2022-03-21 10:54:55 -0300

    Added swagger to user.

--- ms-user/src/main/java/com/user/rest/UserController.java
+++ ms-user/src/main/java/com/user/rest/UserController.java
@@ -2,6 +2,8 @@ package com.user.rest;
 
 import com.user.entity.User;
 import com.user.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -11,27 +13,31 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/user/")
+@Api(value = "users")
 public class UserController {
 
     @Autowired
     UserService userService;
 
+    @ApiOperation(value = "List all users")
     @GetMapping()
     public List<User> findAll() {
         return userService.findAll();
     }
 
+    @ApiOperation(value = "Find a user by id")
     @GetMapping("/{id}")
     public Optional<User> findById(@PathVariable String id) throws Exception {
         return userService.findById(id);
     }
 
+    @ApiOperation(value = "Save a user")
     @PostMapping()
     public User save(@RequestBody User user) {
         return userService.save(user);
     }
 
-
+    @ApiOperation(value = "Find a user with Pagination and Sorting")
     @GetMapping("/paginationAndSort/{offset}/{pageSize}/{field}")
     public Page<User> findWithPaginationAndSorting(@PathVariable int offset, @PathVariable int pageSize, @PathVariable String field) {
         Page<User> usersPagination = userService.findWithPaginationAndSorting(offset, pageSize, field);
